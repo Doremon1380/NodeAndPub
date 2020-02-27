dfaf2659ecc819322f4a09012a00ada60dc31b10
Doremon1380 <46600952+Doremon1380@users.noreply.github.com>
2020-02-27 16:16:40 +0700

    Qua on

--- myserver.js
+++ myserver.js
@@ -7,9 +7,22 @@ var port = process.env.PORT || 2000;
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
+var bodyParser = require('body-Parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
 // khi mo website mac dinh
 app.get("/",(req,res)=>{
     res.render('index');
+})
+
+app.get('/form',(req,res)=>{
+    res.render('form')
+})
+
+app.post('/saveForm',(req,res)=> {
+    // lay thong tin textbox ten la yourname
+    const yourName = req.body.yourName;
+    res.render('confirm',{name:yourName});
 })
 
 var server = app.listen(port,function() {});
